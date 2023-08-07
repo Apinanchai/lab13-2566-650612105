c6ea896b3534d840f6f38338185c4334a5d5ea14
SpookyBoy <110967577+Apinanchai@users.noreply.github.com>
2023-08-07 15:38:38 +0700

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Apinanchai yooyativong"
+            studentId="650612105"
           />
         </div>
       </body>
